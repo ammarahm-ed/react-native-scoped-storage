97f976c5042dceb44ee730467210d522c17f9534
ammarahm-ed <ammarahmed6506@gmail.com>
2022-01-11 15:40:55 +0500

    update example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,59 +1,86 @@
 import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
-import * as ScopedStorage from "react-native-scoped-storage";
+import {TouchableOpacity, View, Text} from 'react-native';
+import * as ScopedStorage from 'react-native-scoped-storage';
 
-const App = () => {
-
-  const Button = ({ title, onPress }: { title: string, onPress: () => void }) => {
-
-    return <TouchableOpacity
+const Button = ({title, onPress}: {title: string; onPress: () => void}) => {
+  return (
+    <TouchableOpacity
       style={{
-        backgroundColor: "green",
+        backgroundColor: 'green',
         padding: 12,
         borderRadius: 5,
-        marginBottom: 10
+        marginBottom: 10,
+        width: '100%',
+        alignItems: 'center',
       }}
-      onPress={onPress}
-    >
+      onPress={onPress}>
       <Text
         style={{
-          color: 'white'
-        }}
-      >
+          color: 'white',
+        }}>
         {title}
       </Text>
     </TouchableOpacity>
-  }
+  );
+};
 
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+      }}>
+      <Button
+        onPress={async () => {
+          try {
+            let dir = await ScopedStorage.openDocumentTree(true);
+            let text = 'Hello world';
 
-  return <View
-    style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}
-  >
-    <Button onPress={async () => {
-      try {
-        console.log(await ScopedStorage.openDocumentTree());
-      } catch (e) {
-        console.log(e)
-      }
-    }}
-      title="openDocumentTree" />
+            await ScopedStorage.writeFile(
+              dir.uri,
+              'helloworld.txt',
+              'text/plain',
+              text,
+            );
+          } catch (e) {
+            console.log(e);
+          }
+        }}
+        title="Select a directory to read/write files"
+      />
 
-    <Button onPress={async () => {
-      try {
-        // Reading an image
-        console.log(await ScopedStorage.openDocument(true,'base64'));
-      } catch (e) {
-        console.log(e)
-      }
-    }}
-      title="openDocument" />
+      <Button
+        onPress={async () => {
+          try {
+            await ScopedStorage.openDocument();
+          } catch (e) {
+            console.log(e);
+          }
+        }}
+        title="Select a file"
+      />
 
-
-  </View>
+      <Button
+        onPress={async () => {
+          try {
+            let file = await ScopedStorage.createDocument(
+              'helloworld.txt',
+              'text/plain',
+              'hello world',
+            );
+            console.log('File saved!',file);
+          } catch (e) {
+            console.log(e);
+          }
+        }}
+        title="Create a new file in user selected directory"
+      />
+    </View>
+  );
 };
 
 export default App;
